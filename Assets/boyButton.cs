61454ae26f6c9828f69e3ed56e9bc632d130ac54
Govini Suhara <govinisuhara@gmail.com>
2022-04-23 16:09:01 +0530

    First four screens

---
+++ Assets/boyButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boyButton : MonoBehaviour
+{
+    public GameObject girlObject;
+
+    public void onPressBoy()
+    {
+        girlObject.SetActive(false); 
+        // centre boy object 
+        // hide button
+        // display score
+        // display playbutton
+
+    }
+
+    
+}
---
+++ Assets/girlButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class girlButton : MonoBehaviour
+{
+    public GameObject boyObject;
+
+    public void onPressGirl()
+    {
+        boyObject.SetActive(false);
+        // centre boy object 
+        // hide button
+        // display score
+        // display playbutton
+
+    }
+
+
+}
+
